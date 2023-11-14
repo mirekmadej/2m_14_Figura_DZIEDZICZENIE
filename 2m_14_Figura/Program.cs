95dda1fa6ddfbb85e84e74b6f2fa36499ef755fa
Mirek Madej <m.madej@zsp3zamosc.pl>
2023-11-14 13:58:42 +0100

    Dodaj pliki projektów.

---
+++ 2m_14_Figura/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Authentication.ExtendedProtection;
+
+namespace _2m_14_Figura
+{
+    class Figura
+    {
+        private string nazwa;
+        public Figura(string nazwa)
+        {
+            this.nazwa = nazwa;
+        }
+        public override string ToString()
+        {
+            return $"{nazwa}";
+        }
+    }
+    class Prostokat : Figura
+    {
+        private double bokA, bokB;
+        public double pole { get { return bokA*bokB; } }
+        public Prostokat(string nazwa,  double bokA, double bokB) :base(nazwa)
+        {
+            this.bokA = bokA;
+            this.bokB = bokB;
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
